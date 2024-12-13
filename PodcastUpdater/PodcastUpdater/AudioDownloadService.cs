39afc3b25c2a988e6e6024b6b82adc1f378315c3
Armand Delessert <armand.delessert@objectis.ch>
2024-12-13 23:45:48 +0100

    Add class `AudioDownloadService`

---
+++ PodcastUpdater/PodcastUpdater/AudioDownloadService.cs
@@ -0,0 +1,19 @@
+﻿namespace PodcastUpdater;
+
+public class AudioDownloadService
+{
+    public string DownloadAudioFromYouTube(string videoUrl)
+    {
+        // Télécharge l'audio de la vidéo avec yt-dlp
+    }
+
+    public string ConvertToMp3(string audioFilePath)
+    {
+        // Convertit le fichier audio en MP3
+    }
+
+    public long GetMp3FileSize(string filePath)
+    {
+        // Retourne la taille du fichier MP3
+    }
+}
