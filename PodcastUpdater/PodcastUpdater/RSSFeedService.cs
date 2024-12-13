90442fef816088aabcbca22e63bd4352b54bce72
Armand Delessert <armand.delessert@objectis.ch>
2024-12-13 23:46:09 +0100

    Add class `RSSFeedService`

---
+++ PodcastUpdater/PodcastUpdater/RSSFeedService.cs
@@ -0,0 +1,14 @@
+﻿namespace PodcastUpdater;
+
+public class RSSFeedService
+{
+    public void UpdateRSSFeed(PodcastEpisode episode)
+    {
+        // Met à jour le fichier XML du flux RSS
+    }
+
+    public string GenerateRSSXml(List<PodcastEpisode> episodes)
+    {
+        // Génère le contenu XML complet du flux RSS
+    }
+}
