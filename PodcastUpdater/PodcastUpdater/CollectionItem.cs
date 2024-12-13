bbb32cf168e43939de01a05374507a6dac3146a5
Armand Delessert <armand.delessert@objectis.ch>
2024-12-13 21:54:31 +0100

    Add class `PodcastEpisode`

--- PodcastUpdater/PodcastUpdater/CollectionItem.cs
+++
@@ -1,8 +0,0 @@
-namespace PodcastUpdater;
-
-public class CollectionItem(int number, string name)
-{
-    public int Number { get; set; } = number;
-
-    public string Name { get; set; } = name;
-}
