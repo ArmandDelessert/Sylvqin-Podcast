f7304da9066ec34c01177d7df7d135cdaefe4db3
Armand Delessert <armand.delessert@objectis.ch>
2024-12-13 23:46:01 +0100

    Add class `GitHubService`

---
+++ PodcastUpdater/PodcastUpdater/GitHubService.cs
@@ -0,0 +1,19 @@
+﻿namespace PodcastUpdater;
+
+public class GitHubService
+{
+    public void UpdateReadme(PodcastEpisode episode)
+    {
+        // Met à jour le fichier README.md avec les informations de l'épisode
+    }
+
+    public void CommitAndPushFile(string filePath, string commitMessage)
+    {
+        // Commit et pousse le fichier sur GitHub
+    }
+
+    public void AddFileToRepository(string filePath)
+    {
+        // Ajoute un fichier au dépôt
+    }
+}
