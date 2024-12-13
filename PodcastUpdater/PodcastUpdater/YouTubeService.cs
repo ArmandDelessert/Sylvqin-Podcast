47562344ca9a9c52bd7941fe61fd8b6e8a0a018c
Armand Delessert <armand.delessert@objectis.ch>
2024-12-13 23:45:35 +0100

    Add class `YouTubeService`

---
+++ PodcastUpdater/PodcastUpdater/YouTubeService.cs
@@ -0,0 +1,19 @@
+﻿namespace PodcastUpdater;
+
+public class YouTubeService
+{
+    public VideoMetadata FetchVideoMetadata(string videoUrl)
+    {
+        // Récupère les métadonnées de la vidéo
+    }
+
+    public DateTime GetPublicationDate(string videoId)
+    {
+        // Obtient la date de publication de la vidéo
+    }
+
+    public void AddVideoToPlaylist(string playlistId, string videoId)
+    {
+        // Ajoute la vidéo à la playlist spécifiée
+    }
+}
